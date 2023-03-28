22467795d98a7a779de842bab7057069cb2f4e69
xzhy324 <xzhy324@qq.com>
2023-03-28 11:57:33 +0800

    feat: add module_locate for syscall_table check

--- sys_inspector/src/syscall_table.c
+++ sys_inspector/src/syscall_table.c
@@ -3,7 +3,7 @@
 #include "core.h"
 #include "util.h"
 
-
+static DEFINE_MUTEX(module_mutex);
 extern unsigned long *sct; /* Syscall Table */
 extern int (*ckt)(unsigned long addr); /* Core Kernel Text */
 
@@ -12,13 +12,26 @@ extern int (*ckt)(unsigned long addr); /* Core Kernel Text */
 void analyze_syscalls(void){
 	int i;
     unsigned long addr;
+    struct module *mod;
+    const char *mod_name;
     printk("[sys_inspector.ko] analyzing syscall!");
 
     for (i=0; i< NR_syscalls; i++){
         addr = sct[i];
         if (!ckt(addr)) {
 			printk(KERN_ALERT"[sys_inspector.ko] syscall[%d] addr:%lx lays in ring3!", i, addr);
+            mutex_lock(&module_mutex);
+            mod = get_module_from_addr(addr);
+            if (mod){
+                printk(KERN_ALERT"[sys_inspector.ko] Module [%s] is hooking!\n",mod->name);
+            } else {
+                mod_name = find_hidden_module(addr);
+                if (mod_name)
+                    printk(KERN_ALERT"[sys_inspector.ko] Hidden Module [%s] is hooking!\n",mod_name);
+            }
+            mutex_unlock(&module_mutex);
 		}
+        
     }
 
 }
